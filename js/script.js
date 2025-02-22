30a67c1b56046e0e0909fb617f0d2422e8f79300
Tamurbek <temuryoldoshev10@gmail.com>
2025-02-22 15:25:51 +0500

    change code

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ import {sayHi as s, sayHi} from './functions.js';
 import {data} from './data.js'
 
 sayHi(data);
+
+let arr=0;
+
+function getArr(){
+    console.log(arr);
+}
+
+console.log(arr);
+arr=5
+getArr();
